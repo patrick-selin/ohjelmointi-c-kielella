01cbe6c5c33738dc98226059733b6ee38a4c7f5a
Patrick Selin <patrick.selin@gmail.com>
2024-04-18 14:09:49 +0300

    L1T4

---
+++ Part 1/L1T4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    char merkki1;
+    char merkki2[21];
+    
+    printf("Anna merkki: ");
+    scanf(" %c", &merkki1);
+    printf("Annoit merkin '%c'.\n", merkki1);
+
+    printf("Anna korkeintaan 20 merkkiä pitkä merkkijono: ");
+    scanf("%20s", merkki2);
+    printf("Annoit merkkijonon '%s'.", merkki2);
+ 
+}
