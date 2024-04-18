e2e15b9792a340b5e6910ee932b57e714a44a5c0
Patrick Selin <patrick.selin@gmail.com>
2024-04-18 15:02:41 +0300

    L2T1

---
+++ Part 2/L2T1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+    int luku1;
+    int luku2;
+    int valinta;
+    int tulos;
+    
+    printf("Anna kaksi kokonaislukua: ");
+    scanf("%d %d", &luku1, &luku2);
+ 
+    printf("\n");
+
+    printf("Valikko\n");
+    printf("1) Summaa luvut yhteen.\n");
+    printf("2) Kerro luvut keskenään.\n");
+
+    printf("Valitse: ");
+    scanf("%d", &valinta);
+
+    printf("\n");
+
+    if (valinta == 1) {
+        tulos = luku1 + luku2;
+        printf("Luvut laskettiin yhteen. Tulos = %d.\n", tulos);
+    } else if (valinta == 2) {
+        tulos = luku1 * luku2;
+        printf("Luvut kerrottiin keskenään. Tulos = %d.\n", tulos);
+    } else {
+        printf("Tuntematon valinta.\n");
+    }
+
+    return 0;
+
+}
