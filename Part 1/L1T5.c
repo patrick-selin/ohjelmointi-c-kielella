782b91fb4a99f6a684324595762b61c3eb660d67
Patrick Selin <patrick.selin@gmail.com>
2024-04-18 14:26:20 +0300

    L1T5

---
+++ Part 1/L1T5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    char etunimi[30];
+    char sukunimi[30];
+    int ika;
+    float paino;
+    
+    printf("Anna etunimesi: ");
+    scanf("%s", etunimi);
+
+    printf("Anna sukunimesi: ");
+    scanf("%s", sukunimi);
+
+    printf("Anna ikäsi: ");
+    scanf("%d", &ika);
+
+    printf("Anna painosi: ");
+    scanf("%f", &paino);
+
+    printf("Nimesi on %s %s, ikäsi on %d vuotta ja painat %.1f kg.", etunimi, sukunimi, ika, paino);
+ 
+}
