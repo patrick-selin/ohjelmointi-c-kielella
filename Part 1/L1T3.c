d1ca39c81673ca69ce73a31290f71e0617cd0365
Patrick Selin <patrick.selin@gmail.com>
2024-04-18 13:40:55 +0300

    L1T3

---
+++ Part 1/L1T3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int luku1;
+    int luku2;
+
+    printf("Anna ensimmäinen kokonaisluku: ");
+    scanf("%d", &luku1);
+
+    printf("Anna toinen kokonaisluku: ");
+    scanf("%d", &luku2);
+
+    printf("(%d + %d) * 2 = %d\n", luku1, luku2, (luku1 + luku2) * 2);
+    printf("(%d / %d) - 3 = %d\n", luku1, luku2, (luku1 / luku2) - 3);
+    printf("%d %% %d = %d\n", luku1 + 1, luku2 - 1, (luku1 + 1) % (luku2 - 1));
+
+    return(0);
+}
