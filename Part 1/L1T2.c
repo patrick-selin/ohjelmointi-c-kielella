29a77541e45f7133b73b4e4ee4a9ce440c539938
Patrick Selin <patrick.selin@gmail.com>
2024-04-18 13:10:33 +0300

    L1T2

---
+++ Part 1/L1T2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int kokonaisluku;
+    float liukuluku;
+
+    printf("Anna kokonaisluku: ");
+    scanf("%d", &kokonaisluku);
+
+    printf("Anna liukuluku: ");
+    scanf("%f", &liukuluku);
+
+    printf("Annoit luvut %d ", kokonaisluku);
+    printf("ja %.2f.", liukuluku);
+
+    return(0);
+}
