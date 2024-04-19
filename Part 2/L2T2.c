8cb438aa2c5e11d8332602504d16767d3cb9a2e1
Patrick Selin <patrick.selin@gmail.com>
2024-04-19 09:31:11 +0300

    L2T2

---
+++ Part 2/L2T2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int iLuku;
+    int iSumma = 0;
+
+    printf("Anna jokin luku väliltä 10 - 200: ");
+    scanf("%d", &iLuku);
+
+    if (iLuku < 10 || iLuku > 200) {
+        printf("Antamasi luku ei ole annetulla välillä.\n");
+    } else {
+        for (int i = 0; i <= iLuku; i++) {
+            iSumma += i;
+        }
+        
+        printf("Lukujen 0 - %d summa on %d.", iLuku, iSumma);
+    }
+
+    return 0;
+}
