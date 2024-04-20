af9d6128b6fbd531ced6e3440c34a8fc4ea0cae2
Patrick Selin <patrick.selin@gmail.com>
2024-04-20 13:49:34 +0300

    L3T2

---
+++ Part 3/L3T2.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NIMI_PITUUS 50
+
+void kirjoitaTiedostoon(const char *tiedostonimi) {
+    char nimi[MAX_NIMI_PITUUS];
+    FILE *tiedosto;
+
+    tiedosto = fopen(tiedostonimi, "a");
+    if (tiedosto == NULL) {
+        perror("Virhe tiedoston avaamisessa");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Anna lisättävä nimi: ");
+    if (fgets(nimi, MAX_NIMI_PITUUS, stdin) == NULL) {
+        printf("Virhe nimen lukemisessa\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(tiedosto, "%s", nimi);
+    fclose(tiedosto);
+    printf("Nimi lisätty tiedostoon.\n");
+    printf("\n");
+}
+
+void lueTiedostosta(const char *tiedostonimi) {
+    char nimi[MAX_NIMI_PITUUS];
+    FILE *tiedosto;
+
+    tiedosto = fopen(tiedostonimi, "r");
+    if (tiedosto == NULL) {
+        perror("Tiedoston avaaminen epäonnistui, lopetetaan:");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Tiedostossa olevat nimet:\n");
+    while (fgets(nimi, MAX_NIMI_PITUUS, tiedosto) != NULL) {
+        printf("%s", nimi);
+    }
+
+    fclose(tiedosto);
+    printf("Tiedosto luettu ja tulostettu.\n");
+    printf("\n");
+}
+
+void pyoritaValikkoa(const char *tiedostonimi) {
+    int valinta;
+    while (1) {
+        printf("Valitse alla olevista valinnoista\n");
+        printf("1) Lisää uusi nimi\n");
+        printf("2) Tulosta nimet\n");
+        printf("0) Lopeta\n");
+        printf("Anna valintasi: ");
+        scanf("%d", &valinta);
+
+        if (valinta == 1) {
+            getchar();
+            kirjoitaTiedostoon(tiedostonimi);
+        } else if (valinta == 2) {
+            lueTiedostosta(tiedostonimi);
+        } else if (valinta == 0) {
+            break; 
+        } else {
+            printf("Tuntematon valinta. Yritä uudelleen.\n");
+        }
+    }
+    printf("Kiitos ohjelman käytöstä.\n");
+}
+
+int main() {
+    char tiedostonimi[100];
+
+    printf("Anna käsiteltävän tiedoston nimi: ");
+    scanf("%99s", tiedostonimi);
+
+    pyoritaValikkoa(tiedostonimi);
+
+    return 0;
+}
