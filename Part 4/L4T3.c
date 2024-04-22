afabdc95ac3246c2e4317d457d3ef37d5872094c
Patrick Selin <patrick.selin@gmail.com>
2024-04-22 17:23:16 +0300

    L4T4.c

---
+++ Part 4/L4T3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int iArch, char *argv[]) {
+    if (iArch < 4) {
+        printf("Et antanut syötteitä.\n");
+        return 1;
+    }
+
+    float fNumero1 = atof(argv[1]);
+    char cOperand = argv[2][0];
+    float fNumero2 = atof(argv[3]);
+
+    printf("%.2f %c %.2f = ", fNumero1, cOperand, fNumero2);
+    switch (cOperand) {
+        case '+':
+            printf("%.2f\n", fNumero1 + fNumero2);
+            break;
+        case '-':
+            printf("%.2f\n", fNumero1 - fNumero2);
+            break;
+        case 'x':
+            printf("%.2f\n", fNumero1 * fNumero2);
+            break;
+        case '/':
+            if (fNumero2 != 0) {
+                printf("%.2f\n", fNumero1 / fNumero2);
+            } else {
+                printf("Virheellinen syöte.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Virheellinen syöte.\n");
+            return 1;
+    }
+    printf("Kiitos ohjelman käytöstä.");
+
+    return 0;
+}
