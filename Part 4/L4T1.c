6b46a1077574b06d0362c7e36158ecde079cbd2c
Patrick Selin <patrick.selin@gmail.com>
2024-04-22 13:58:10 +0300

    L4T3.c

--- Part 4/L4T1.c
+++ Part 4/L4T1.c
@@ -1,1 +1,32 @@
-// todo
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NIMEN_PITUUS 50
+
+struct Kirja {
+    char aNimi[MAX_NIMEN_PITUUS + 1];
+    int iPainovuosi;
+    int iHyllypaikka;
+};
+
+int main() {
+    struct Kirja kirja1;
+
+    printf("Anna kirjan nimi: ");
+    fgets(kirja1.aNimi, sizeof(kirja1.aNimi), stdin);
+    if (kirja1.aNimi[strlen(kirja1.aNimi) - 1] == '\n') {
+        kirja1.aNimi[strlen(kirja1.aNimi) - 1] = '\0';
+    }
+
+    printf("Anna kirjan painovuosi: ");
+    scanf("%d", &kirja1.iPainovuosi);
+
+    printf("Anna kirjan hyllypaikka: ");
+    scanf("%d", &kirja1.iHyllypaikka);
+
+    printf("Kirjan nimi on '%s', painovuosi %d ja hyllypaikka %d.\n", kirja1.aNimi, kirja1.iPainovuosi, kirja1.iHyllypaikka);
+    printf("Kiitos ohjelman käytöstä.\n");
+
+    return 0;
+}
---
+++ Part 4/L4T2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#define RIVI 2
+#define SARAKE 2
+
+void kysyMatriisi(int matriisi[RIVI][SARAKE], char nimi[]) {
+    printf("Matriisin %s alkiot:\n", nimi);
+    for (int i = 0; i < RIVI; i++) {
+        for (int j = 0; j < SARAKE; j++) {
+            printf("Rivi %d alkio %d: ", i + 1, j + 1);
+            scanf("%d", &matriisi[i][j]);
+        }
+    }
+}
+
+void tulostaMatriisi(int matriisi[RIVI][SARAKE], char nimi[]) {
+    printf("%s:\n", nimi);
+    for (int i = 0; i < RIVI; i++) {
+        for (int j = 0; j < SARAKE; j++) {
+            printf("%4d ", matriisi[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+void laskeSummaMatriisi(int iMatriisi[RIVI][SARAKE], int iMatriisi2[RIVI][SARAKE], int summa[RIVI][SARAKE]) {
+    for (int i = 0; i < RIVI; i++) {
+        for (int j = 0; j < SARAKE; j++) {
+            summa[i][j] = iMatriisi[i][j] + iMatriisi2[i][j];
+        }
+    }
+}
+
+int main() {
+    int iMatriisi[RIVI][SARAKE], iMatriisi2[RIVI][SARAKE], summa[RIVI][SARAKE];
+
+    printf("Anna arvot yhteenlaskettaville matriiseille:\n");
+
+    kysyMatriisi(iMatriisi, "1");
+    printf("\n");
+    tulostaMatriisi(iMatriisi, "Matriisi 1");
+    printf("\n");
+
+    kysyMatriisi(iMatriisi2, "2");
+    printf("\n");
+    tulostaMatriisi(iMatriisi2, "Matriisi 2");
+    
+    printf("\n");
+
+    laskeSummaMatriisi(iMatriisi, iMatriisi2, summa);
+
+    tulostaMatriisi(summa, "Summamatriisi");
+
+    printf("\nKiitos ohjelman käytöstä.");
+
+    return 0;
+}
