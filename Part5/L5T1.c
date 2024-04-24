1d53f1bf0e2f5eac796e66db8245a533da367186
Patrick Selin <patrick.selin@gmail.com>
2024-04-24 10:23:02 +0300

    L5T1

--- Part5/L5T1.c
+++ Part5/L5T1.c
@@ -1,1 +1,25 @@
-// todo
+#include <stdio.h>
+#include <stdlib.h>
+
+void laskeNelio(int *iLuku) {
+    *iLuku *= *iLuku;
+}
+
+int main(int iArgs, char *cArgv[]) {
+    if (iArgs != 2) {
+        printf("Et antanut lukua!\n");
+        printf("Kiitos ohjelman käytöstä.");
+        return 0;
+    }
+
+    int iLuku = atoi(cArgv[1]);
+
+    printf("Luku on %d.\n", iLuku);
+
+    laskeNelio(&iLuku);
+
+    printf("Luvun neliö on %d.\n", iLuku);
+    printf("Kiitos ohjelman käytöstä.");
+
+    return 0;
+}
