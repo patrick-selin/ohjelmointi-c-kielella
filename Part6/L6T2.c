2882eb75e2faaf2e658b9db928f5ba50ee2af097
Patrick Selin <patrick.selin@gmail.com>
2024-04-26 14:56:27 +0300

    L6T2 ok

--- Part6/L6T2.c
+++ Part6/L6T2.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct AutonTiedot {
+struct AutoNode {
     char pMerkki[50];
     int iVuosiLuku;
-    struct AutonTiedot *pSeuraava;
+    struct AutoNode *pSeuraava;
 };
 
-struct AutonTiedot *lisaaAuto(struct AutonTiedot *pAlku, char *pMerkki, int iValmistusVuosi) {
-    struct AutonTiedot *pUusiAuto = NULL;
+struct AutoNode *lisaaAuto(struct AutoNode *pAlku, char *pMerkki, int iValmistusVuosi) {
+    struct AutoNode *pUusiAuto = NULL;
     
-    if ((pUusiAuto = (struct AutonTiedot *)malloc(sizeof(struct AutonTiedot))) == NULL) {
+    if ((pUusiAuto = (struct AutoNode *)malloc(sizeof(struct AutoNode))) == NULL) {
         perror("Muistin varaus epäonnistui");
         exit(0);
     }
@@ -23,7 +23,7 @@ struct AutonTiedot *lisaaAuto(struct AutonTiedot *pAlku, char *pMerkki, int iVal
     if (pAlku == NULL) {
         return pUusiAuto;
     } else {
-        struct AutonTiedot *pTemppi = pAlku;
+        struct AutoNode *pTemppi = pAlku;
         while (pTemppi->pSeuraava != NULL) {
             pTemppi = pTemppi->pSeuraava;
         }
@@ -32,72 +32,71 @@ struct AutonTiedot *lisaaAuto(struct AutonTiedot *pAlku, char *pMerkki, int iVal
     }
 }
 
-
-void tulostaAutot(struct AutonTiedot *pAlku) {
+void tulostaAutot(struct AutoNode *pAlku) {
     if (pAlku == NULL) {
         printf("Ajoneuvoja ei löytynyt.\n");
         return;
     }
 
-    struct AutonTiedot *pTemppi = pAlku;
-    int i = 1;
+    struct AutoNode *pTemppi = pAlku;
+    int iTulosteNumero = 1;
+
     while (pTemppi != NULL) {
-        printf("%d. ajoneuvo: %s vuosimalli %d.\n", i, pTemppi->pMerkki, pTemppi->iVuosiLuku);
+        printf("%d. ajoneuvo: %s vuosimalli %d.\n", iTulosteNumero, pTemppi->pMerkki, pTemppi->iVuosiLuku);
         pTemppi = pTemppi->pSeuraava;
-        i++;
+        iTulosteNumero++;
     }
 }
 
-void vapautaMuisti(struct AutonTiedot *pAlku) {
-    struct AutonTiedot *pTemppi = pAlku;
+void vapautaMuisti(struct AutoNode *pAlku) {
+    struct AutoNode *pTemppi = pAlku;
 
     while (pTemppi != NULL) {
-        struct AutonTiedot *pSeuraava = pTemppi->pSeuraava;
+        struct AutoNode *pSeuraava = pTemppi->pSeuraava;
         free(pTemppi);
         pTemppi = pSeuraava;
     }
+    printf("Muisti vapautettu.\n");
 }
 
 // ******* MAIN
 
 int main(int argc, char *argv[]) {
-    struct AutonTiedot *pAlku = NULL;
+    struct AutoNode *pAlku = NULL;
     char pMerkki[50];
     int iValmistusVuosi;
     int iOnLuettu = 0;
 
     if (argc < 2) {
         printf("Et antanut tiedoston nimeä.\n");
-        return 1;
+        printf("Kiitos ohjelman käytöstä.");
+        return 0;
     }
 
     char *ptiedostonNimi = argv[1];
 
     FILE *pTiedosto = fopen(ptiedostonNimi, "r");
+    printf("Luetaan tiedot tiedostosta %s.\n", ptiedostonNimi);
+
     if (pTiedosto == NULL) {
         printf("Tiedoston avaus epäonnistui.\n");
-        return 1;
+        printf("Kiitos ohjelman käytöstä.");
+        return 0;
     }
 
-    printf("Luetaan tiedot tiedostosta %s.\n", ptiedostonNimi);
 
     // lue ja lisää
     while (fscanf(pTiedosto, "%s %d", pMerkki, &iValmistusVuosi) == 2) {
         pAlku = lisaaAuto(pAlku, pMerkki, iValmistusVuosi);
         iOnLuettu = 1;
     }
-
     fclose(pTiedosto);
 
-    if (iOnLuettu == 1) {
-        printf("Tiedot luettu linkitettyyn listaan.\n");
-        tulostaAutot(pAlku);
-        vapautaMuisti(pAlku);
-    } else {
-        printf("Tiedostossa ei ollut tietoja.\n");
-    }
+    printf("Tiedot luettu linkitettyyn listaan.\n");
+    tulostaAutot(pAlku);
+    vapautaMuisti(pAlku);
 
-    printf("Kiitos ohjelman käytöstä.\n");
+    printf("Kiitos ohjelman käytöstä.");
 
     return 0;
 }
