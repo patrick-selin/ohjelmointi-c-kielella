eadf97de035a62c39a9741d2b41a2bca7ea2097d
Patrick Selin <patrick.selin@gmail.com>
2024-04-26 12:53:42 +0300

    L6T1

--- Part6/L6T1.c
+++ Part6/L6T1.c
@@ -1,1 +1,61 @@
 // L6T1: Linkitetyn listan perusoperaatiot
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int iKokonaisLuku;
+    struct Node* pSeuraava;
+};
+
+int main() {
+    struct Node* pAlku = NULL;
+    struct Node* pLoppu = NULL;
+    struct Node* pUusi = NULL;
+    int iValinta;
+
+    printf("Anna listan luvut.\n");
+
+    do {
+        printf("Anna positiivinen kokonaisluku, 0 lopettaa: ");
+        scanf("%d", &iValinta);
+
+        if (iValinta != 0) {
+            if ((pUusi = (struct Node*)malloc(sizeof(struct Node))) == NULL) {
+                perror("Muistin varaus epäonnistui");
+                exit(0);
+            }
+
+            // luo uusi node
+            pUusi->iKokonaisLuku = iValinta;
+            pUusi->pSeuraava = NULL;
+
+            if (pAlku == NULL) {
+                pAlku = pUusi;
+                pLoppu = pUusi;
+            } else {
+                pLoppu->pSeuraava = pUusi;
+                pLoppu = pUusi;
+            }
+        }
+    } while (iValinta != 0);
+
+    printf("Listassa on seuraavat luvut: ");
+    struct Node* pTemp = pAlku;
+    while (pTemp != NULL) {
+        printf("%d ", pTemp->iKokonaisLuku);
+        pTemp = pTemp->pSeuraava;
+    }
+    printf("\n");
+
+    while (pAlku != NULL) {
+        struct Node* pTemp = pAlku;
+        pAlku = pAlku->pSeuraava;
+
+        free(pTemp);
+    }
+
+    printf("Kiitos ohjelman käytöstä.");
+
+    return 0;
+}
